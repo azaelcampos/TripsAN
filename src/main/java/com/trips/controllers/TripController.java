5837b5deb74796faab026758bf70d9379fe44c6a
Azael Alberto Campos Lopez <ia.azaelcampos@ufg.edu.sv>
2026-04-25 12:35:10 -0600

    Vista home. ID. en lab

--- src/main/java/com/trips/controllers/TripController.java
+++ src/main/java/com/trips/controllers/TripController.java
@@ -1,5 +1,6 @@
 package com.trips.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,18 +8,26 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.trips.models.Trip;
+import com.trips.services.ITripServices;
+
 @Controller
 @RequestMapping("/trips")
 public class TripController {
 	
+	
+	@Autowired
+	private ITripServices servicesTrip;
+	
+	@GetMapping("/view/{id}")
+	public String verDetalle(@PathVariable("id") int idTrip, Model model) {
+	    System.out.println("IdTrip es: " + idTrip);
+	    model.addAttribute ("idTrip" , idTrip);
+	    
+	    return "trips/detalle";
+	    }
 
-@GetMapping("/view/{id}")
-public String verDetalle(@PathVariable("id") int idTrip, Model model) {
-    System.out.println("IdTrip es: " + idTrip);
-    model.addAttribute ("idTrip" , idTrip);
-    
-    return "trips/detalle";
-    }
+
 
 @GetMapping("/delete")
 public String eliminar(@RequestParam("id") int idTrip, Model model) {
@@ -26,4 +35,5 @@ public String eliminar(@RequestParam("id") int idTrip, Model model) {
     model.addAttribute("id", idTrip);
     return "mensaje"; 
     }
+
 }
